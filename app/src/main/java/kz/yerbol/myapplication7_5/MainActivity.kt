5e138189f98a4b88517dcc57570ba59815c47b7a
Yerbol Home <kabzitov.erbol@gmail.com>
2023-11-18 19:52:54 +0600

    Домашнее задание. Урок 7

--- app/src/main/java/kz/yerbol/myapplication7_5/MainActivity.kt
+++ app/src/main/java/kz/yerbol/myapplication7_5/MainActivity.kt
@@ -1,5 +1,7 @@
 package kz.yerbol.myapplication7_5
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,6 +12,8 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
     private lateinit var tvPin: TextView
+    private val KEY_PIN = "key_pin"
+    private val KEY_TV_PIN_COLOR = "key_tv_pin_color"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +34,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun checkPin(view: View) {
-        if (!tvPin.text.equals("1567")) return
-        Toast.makeText(applicationContext, "Success!", Toast.LENGTH_SHORT).show()
+        if (tvPin.text.equals("1567")) {
+            tvPin.setTextColor(Color.GREEN)
+            Toast.makeText(applicationContext, "Success!", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, SecondActivity::class.java))
+        } else {
+            tvPin.setTextColor(Color.RED)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(KEY_PIN, tvPin.text.toString())
+        outState.putInt(KEY_TV_PIN_COLOR, tvPin.currentTextColor)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        tvPin.text = savedInstanceState.getString(KEY_PIN)
+        tvPin.setTextColor(savedInstanceState.getInt(KEY_TV_PIN_COLOR))
     }
 }
